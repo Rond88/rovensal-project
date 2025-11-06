e6c7395dc3f2bee3ca2ff48e6600798a5d5dc402
“rond88” <r0nald19202006@gmail.com>
2025-11-06 17:37:58 +0100

    Refactor ReynaComponent: enhance product loading and search functionality, update template structure

--- src/app/entregas/reyna/components/reynaComponent/reynaComponent.ts
+++ src/app/entregas/reyna/components/reynaComponent/reynaComponent.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { ProductCardUnroutedComponent } from '../product-card-unrouted/product-card-unrouted';
+import { Product } from '../../model/product.model';
+import { ProductService } from '../../service/product.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reyna',
-  imports: [],
+  imports: [ProductCardUnroutedComponent, FormsModule],
   templateUrl: './reynaComponent.html',
   styleUrl: './reynaComponent.css',
-  standalone: true
+  standalone: true,
 })
-export class ReynaComponent {}
+export class ReynaComponent {
+  products: Product[] = [];
+  busqueda: string = '';
+
+  constructor(private productService: ProductService) {}
+
+   ngOnInit(): void {
+     this.loadProducts();
+  }
+  private loadProducts(): void {
+    this.productService.getAllProducts().subscribe({
+      next: (products) => (this.products = products),
+      error: (err) => console.error('Error cargando productos', err),
+    });
+  }
+  
+  get filtrarProducts(): Product[] {
+    const term = this.busqueda.toLowerCase();
+    return this.products.filter((p) =>
+      p.title.toLowerCase().includes(term)
+    );
+  }
+}
